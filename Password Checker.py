886969c4a2665c1223965940f250f5fff641f23d
thantology <42950610+thantology@users.noreply.github.com>
2021-08-07 18:56:03 -0400

    Update Password Checker.py

--- Password Checker.py
+++ Password Checker.py
@@ -1,7 +1,32 @@
-height= input("Enter your height in m "))
-weight = int(input ("Enter your weight in kg "))
+#Author: Jonathan Anthony Hernandez
+#Password Checker Program
+#Description: Easy Password Checker using list, if-else, and for loop
 
-BMI = weight/(height **2)
 
-print(BMI)
+Lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+Upper_letters= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+','_']
 
+
+userpassword = str(input("Type your password "));
+
+lower_letter_password = 0;
+upper_letter_password = 0;
+number_password=0;
+symbols_password=0;
+
+if len(userpassword) >= 6:
+    for values in userpassword:
+        if values in Lower_letters:
+            lower_letter_password+=1;
+        elif values in Upper_letters:
+            upper_letter_password+=1;
+        elif values in numbers:
+            number_password+=1;
+        elif values in symbols:
+            symbols_password+=1;
+    print (f"Your password has {upper_letter_password} upper case letters, {lower_letter_password} lower letters, {number_password} numbers, {symbols_password} symbols")
+
+else:
+    print ("You password is not sufficient for our system")
